fa2b32ed62857d51a5a3bc22397f240dde869b61
Vasily Rokhlin <rokhlin.vasily@yandex.com>
2023-01-28 18:13:16 +0300

    Added JUnit5 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,14 @@
 package ru.netology.service;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService cashbackHackService = new CashbackHackService();
 
+    //JUnit4
     @Test
     public void shouldCountRemain() {
         int amount = 1900;
@@ -23,5 +25,25 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
 
         Assert.assertEquals(expected, actual);
+    }
+
+
+    //JUnit5
+    @org.junit.jupiter.api.Test
+    public void shouldCountRemain2() {
+        int amount = 1900;
+        int expected = 100;
+        int actual = cashbackHackService.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldCountRemainInRoundValues2() {
+        int amount = 2000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
